719c04cfe42bb0945773533782023a0140c90d61
jhonatanz <jhonatanz@gmail.com>
2019-12-03 19:56:57 -0500

    Primer test de ichimoku

--- ichimoku_test.R
+++ ichimoku_test.R
@@ -10,13 +10,13 @@ tenkan <- vector(mode = "numeric")
 kijun <- vector(mode = "numeric")
 senkou_B <- vector(mode = "numeric")
 i<-length(df_A$Precio_Cierre)
-while(i>52){
+while(i>26){
   x<-(max(df_A$Precio_Mayor[(i-9):i])+min(df_A$Precio_Menor[(i-9):i]))/2
   tenkan<-c(tenkan, x)
   i<-i-1
 }
 i<-length(df_A$Precio_Cierre)
-while(i>52){
+while(i>26){
   x<-(max(df_A$Precio_Mayor[(i-26):i])+min(df_A$Precio_Menor[(i-26):i]))/2
   kijun<-c(kijun, x)
   i<-i-1
@@ -28,3 +28,36 @@ while(i>52){
   i<-i-1
 }
 senkou_A <- (tenkan+kijun)/2
+chikou<-vector(mode = "numeric", length = length(df_A$Precio_Cierre))
+chikou[1:(length(chikou)-26)]<-df_A$Precio_Cierre[26:length(chikou)]
+# el chikou se tiene que correr 26 dias hacia atras
+
+# estandarizacion de tamaños de los vectores del ichimoku
+tenkan_sen <- c(vector(mode = "numeric", length = 26), tenkan, vector(mode = "numeric", length = 26))
+kijun_sen <- c(vector(mode = "numeric", length = 26), kijun, vector(mode = "numeric", length = 26))
+senkou_span_B <- c(vector(mode = "numeric", length = 78), senkou_B)
+senkou_span_A <- c(vector(mode = "numeric", length = 52), senkou_A)
+chikou_span <- c(chikou, vector(mode = "numeric", length = 26))
+# construccion del vector de fechas
+dates <- vector(length = 26)
+dates[1:26] <- df_A$fecha[length(df_A$fecha)]
+dates2 <- 1:26
+dates1 <- dates+dates2
+dates3 <- c(df_A$fecha, dates1)
+# Vector de los precios de cierre
+cierre <- df_A$Precio_Cierre
+cierre <- c(cierre, vector(length = 26))
+# Construccion del data frame, se reemplazan los ceros por NA para que no sean graficados
+df_ichi <- data.frame(fechas = dates3, p_cierre = cierre, TS = tenkan_sen, KS = kijun_sen, 
+                      SSA = senkou_span_A, SSB = senkou_span_B, CS =chikou_span)
+df_ichi[df_ichi==0] <- NA
+# Construccion de graficas
+grafica <- ggplot(data = df_ichi)
+grafica <- grafica + geom_line(mapping = aes(x = fechas, y = p_cierre), colour = "BLACK")
+grafica <- grafica + geom_line(mapping = aes(x = fechas, y = TS), colour = "BLUE")
+grafica <- grafica + geom_line(mapping = aes(x = fechas, y = KS), colour = "MAGENTA")
+grafica <- grafica + geom_line(mapping = aes(x = fechas, y = SSA), colour = "GREEN")
+grafica <- grafica + geom_line(mapping = aes(x = fechas, y = SSB), colour = "RED")
+grafica <- grafica + geom_line(mapping = aes(x = fechas, y = CS), colour = "YELLOW")
+print(grafica)
+  
