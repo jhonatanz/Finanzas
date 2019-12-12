268174477fad609b0412e930556177bf5a95c58d
jhonatanz <jhonatanz@gmail.com>
2019-12-11 21:53:22 -0500

    correccion del sentido de las graficas

--- ichimoku_test.R
+++ ichimoku_test.R
@@ -1,47 +1,48 @@
 library(DBI)
 library(RMySQL)
 library(dplyr)
+library(ggplot2)
 con <- dbConnect(MySQL(), group = "bolsa")
 df <- dbGetQuery(con, "SELECT * FROM valores")
 dbDisconnect(con)
 df$fecha <- as.Date(df$fecha)
 df_A <- filter(df, Nemotecnico == 'BCOLOMBIA')
-tenkan <- vector(mode = "numeric")
-kijun <- vector(mode = "numeric")
-senkou_B <- vector(mode = "numeric")
 i<-length(df_A$Precio_Cierre)
-while(i>26){
-  x<-(max(df_A$Precio_Mayor[(i-9):i])+min(df_A$Precio_Menor[(i-9):i]))/2
-  tenkan<-c(tenkan, x)
+tenkan <- vector(mode = "numeric", length = i)
+kijun <- vector(mode = "numeric", length = i)
+senkou_B <- vector(mode = "numeric", length = i)
+while(i>9){
+  tenkan[i]<-(max(df_A$Precio_Mayor[(i-9):i])+min(df_A$Precio_Menor[(i-9):i]))/2
   i<-i-1
 }
 i<-length(df_A$Precio_Cierre)
 while(i>26){
-  x<-(max(df_A$Precio_Mayor[(i-26):i])+min(df_A$Precio_Menor[(i-26):i]))/2
-  kijun<-c(kijun, x)
+  kijun[i]<-(max(df_A$Precio_Mayor[(i-26):i])+min(df_A$Precio_Menor[(i-26):i]))/2
   i<-i-1
 }
 i<-length(df_A$Precio_Cierre)
 while(i>52){
-  x<-(max(df_A$Precio_Mayor[(i-52):i])+min(df_A$Precio_Menor[(i-52):i]))/2
-  senkou_B<-c(senkou_B, x)
+  senkou_B[i]<-(max(df_A$Precio_Mayor[(i-52):i])+min(df_A$Precio_Menor[(i-52):i]))/2
   i<-i-1
 }
 senkou_A <- (tenkan+kijun)/2
+senkou_A[1:52]<-0
 chikou<-vector(mode = "numeric", length = length(df_A$Precio_Cierre))
 chikou[1:(length(chikou)-26)]<-df_A$Precio_Cierre[26:length(chikou)]
 # el chikou se tiene que correr 26 dias hacia atras
 
 # estandarizacion de tamaños de los vectores del ichimoku
-tenkan_sen <- c(vector(mode = "numeric", length = 26), tenkan, vector(mode = "numeric", length = 26))
-kijun_sen <- c(vector(mode = "numeric", length = 26), kijun, vector(mode = "numeric", length = 26))
-senkou_span_B <- c(vector(mode = "numeric", length = 78), senkou_B)
-senkou_span_A <- c(vector(mode = "numeric", length = 52), senkou_A)
+tenkan_sen <- c(tenkan, vector(mode = "numeric", length = 26))
+kijun_sen <- c(kijun, vector(mode = "numeric", length = 26))
+senkou_span_B <- c(vector(mode = "numeric", length = 26), senkou_B)
+senkou_span_A <- c(vector(mode = "numeric", length = 26), senkou_A)
 chikou_span <- c(chikou, vector(mode = "numeric", length = 26))
 # construccion del vector de fechas
 dates <- vector(length = 26)
+#se asigna la ultima fecha al vector "dates"
 dates[1:26] <- df_A$fecha[length(df_A$fecha)]
 dates2 <- 1:26
+#se generan 26 fechas en el futuro
 dates1 <- dates+dates2
 dates3 <- c(df_A$fecha, dates1)
 # Vector de los precios de cierre
@@ -52,7 +53,7 @@ df_ichi <- data.frame(fechas = dates3, p_cierre = cierre, TS = tenkan_sen, KS = 
                       SSA = senkou_span_A, SSB = senkou_span_B, CS =chikou_span)
 df_ichi[df_ichi==0] <- NA
 # Construccion de graficas
-grafica <- ggplot(data = df_ichi)
+grafica <- ggplot(data = df_ichi[52:length(df_ichi$fechas), ])
 grafica <- grafica + geom_line(mapping = aes(x = fechas, y = p_cierre), colour = "BLACK")
 grafica <- grafica + geom_line(mapping = aes(x = fechas, y = TS), colour = "BLUE")
 grafica <- grafica + geom_line(mapping = aes(x = fechas, y = KS), colour = "MAGENTA")
